e68c3caff8a3c64c6ed81e9e21712b18dcb5d0ef
joon <pirxthepilot@users.noreply.github.com>
2018-07-13 23:41:12 -0700

    Makefile and versioning

--- {{cookiecutter.project_name}}/main.go
+++ {{cookiecutter.project_name}}/main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 )
 
+var version = "undefined"
+
+// Main
 func main() {
-	fmt.Println("{{cookiecutter.project_name}}")
+
+	// Flags processing
+	versionFlag := flag.Bool("version", false, "Show version")
+	flag.Parse()
+
+	// Version
+	if *versionFlag {
+		fmt.Println(version)
+		os.Exit(0)
+	}
+
 }
